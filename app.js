b7f75a8ff3c3ae0f991109db533db86b1f787087
Zack Lee <zacklee@Zacks-MacBook-Pro-2.local>
2023-02-28 16:54:05 -0800

    added port env

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use((error, req, res, next) => {
 mongoose.set("strictQuery", true);
 mongoose
   .connect(URL)
-  .then(() => app.listen(5000))
+  .then(() => app.listen(process.env.PORT || 5000))
   .catch((err) => {
     console.log(err);
   });
